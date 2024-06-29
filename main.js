d5f0b7d2e88e7b5695631b6e42d1a658fcb43700
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-06-29 08:57:36 +0200

    Rewrote main to use crawl

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+import { argv, exit } from 'node:process';
+import { crawlPage } from './crawl.js';
+
+async function main() {
+  if (argv.length !== 3) {
+    console.log("Only 1 argument!");
+    exit(1);
+  }
+
+  console.log(`BaseURL is ${argv[2]}`);
+  await crawlPage(argv[2]);
+}
+
+await main();
+
